69608f6b701c2dbe235a357bfc72dd7cfe45f798
Caleb-Campbell-1 <caleb.campbell@stu.ocsb.ca>
2025-12-07 17:06:58 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,14 +4,12 @@
  * Created on: Dec 2025
  * This program moves a sprite around the outside of a square
 */
-let loopCounter = 0
-let loopCounter2 = 0
 let sprite: game.LedSprite = null
 sprite = game.createSprite(0, 0)
 
 input.onButtonPressed(Button.A, function () {
-    for (let loopCounter2 = 0; loopCounter2 < 4; loopCounter2++) {
-        for (let loopCounter = 0; loopCounter < 5; loopCounter++) {
+    for (let index = 0; index < 4; index++) {
+        for (let index = 0; index < 5; index++) {
             sprite.move(1)
             basic.pause(500)
         }
