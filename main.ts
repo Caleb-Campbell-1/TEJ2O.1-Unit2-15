b3e3239eca29d5c60bbb7f91a49a1a31572ceec0
Caleb-Campbell-1 <caleb.campbell@stu.ocsb.ca>
2025-12-07 16:56:20 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Caleb Campbell
+ * Created on: Dec 2025
+ * This program moves a sprite around the outside of a square
 */
+let loopCounter = 0
+let loopCounter2 = 0
+let sprite: game.LedSprite = null
+sprite = game.createSprite(0, 0)
+loopCounter = 0
+loopCounter2 = 0
 
-basic.showString('Hello, World!')
+input.onButtonPressed(Button.A, function () {
+    for (let loopCounter2 = 0; loopCounter2 < 4; loopCounter2++) {
+        for (let loopCounter = 0; loopCounter < 5; loopCounter++) {
+            sprite.move(1)
+            basic.pause(500)
+        }
+        sprite.turn(Direction.Right, 90)
+        basic.pause(500)
+    }
+})
+
